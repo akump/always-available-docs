47dff8356f45615d40dd57b8670bb759da38fcd6
akump <kumpandrew@gmail.com>
2023-06-17 14:22:40 -0400

    Docs prototype

---
+++ README.md
@@ -0,0 +1,1 @@
+https://andrews-recipes.surge.sh/
---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import recipes from "./recipes";
+  let reversedRecipes = recipes.reverse();
+</script>
+
+<main>
+  <h1 class="header">MS Teams: Always Available Docs</h1>
+
+</main>
+
+<style>
+  main {
+    padding-left: 8px;
+    padding-bottom: 50px;
+    background-color: 37383a;
+  }
+
+  .header {
+    color: #7b83eb;
+    font-size: 4em;
+    font-weight: 100;
+    margin-top: 0;
+    margin-bottom: 1rem;
+  }
+</style>
